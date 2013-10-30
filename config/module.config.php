4bdc404bf57eadd14c682208cdccd36960b03005
croluc <croluc@hotmail.com>
2013-10-30 10:45:08 +0100

    Create module.config.php

---
+++ config/module.config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'view_helpers' => array(
+        'invokables' => array(
+            'displayLangForm' => 'Lang\ViewHelper\DisplayLangForm',
+        ),
+    ),
+);
+
+?>
