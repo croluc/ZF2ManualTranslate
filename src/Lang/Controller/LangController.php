9b215a686963c5bd28a41f13acf9d058adcabe26
croluc <croluc@hotmail.com>
2013-10-31 12:25:08 +0100

    Update LangController.php

--- src/Lang/Controller/LangController.php
+++ src/Lang/Controller/LangController.php
@@ -3,14 +3,15 @@
 namespace Lang\Controller;
 
 use Lang\Model\LangSession;
+use Zend\Http\Request;
 
 class LangController{
     
-    public function __construct($translator,$request) {
+    public function __construct($translator,Request $request) {
         
         $session = new LangSession();
-        
-        if($request->isPost()){
+        if($request->isPost()&&$request->getPost()->get('lang')!=""){
+            
             $lang = $request->getPost()->get('lang');
             $translator->setLocale($request->getPost()->get('lang'));
             $session->storage->setMetadata('lang', $lang);
@@ -23,7 +24,6 @@ class LangController{
                 $session->storage->setMetadata('lang',$lang);
             }
         }
-        
     }
 }
 
